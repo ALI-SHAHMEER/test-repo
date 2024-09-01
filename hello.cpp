923da46dbc849b9461bc42f58349fd7b4109bdc0
Ali Shahmeer <areo5459@gmail.com>
2024-09-01 15:21:14 +0500

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -9,5 +9,9 @@ int main(){
     cout<<"Address of varible word is "<<&word<<endl;
     cout<<*ptr<<endl;
     cout<<"Address of pointer ptr is "<<&ptr<<endl;
+    for(int i=0;i>=10;i++)
+    {
+        cout<<i<<endl;
+    }
     return 0;
 }
